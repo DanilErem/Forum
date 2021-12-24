63dfb83f29159c5a2d028058d432cf23cc48764f
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 20:16:10 +0200

    Update registration-reducer.js

--- frontend/src/store/registration-reducer.js
+++ frontend/src/store/registration-reducer.js
@@ -1,11 +1,13 @@
 let registrationPage = {
     nicknameInputText : "",
     passwordInputText : "",
+    inputAttitude : "password",
 }
 const CHANGE_NICKNAME_INPUT_TEXT_ACTION = "CHANGE_NICKNAME_INPUT_TEXT";
 const CHANGE_PASSWORD_INPUT_ACTION =  "CHANGE_PASSWORD_INPUT";
+const CHANGE_VISIBLE_ACTION = "CHANGE_VISIBLE";
 export function registrationReducer(state=registrationPage, action) {
-    debugger;
+
     switch (action.type) {
         case CHANGE_NICKNAME_INPUT_TEXT_ACTION:
             state.nicknameInputText = action.newText;
@@ -13,6 +15,13 @@ export function registrationReducer(state=registrationPage, action) {
         case CHANGE_PASSWORD_INPUT_ACTION:
             state.passwordInputText = action.newText;
             return state;
+        case CHANGE_VISIBLE_ACTION:
+            if (state.inputAttitude === "password"){
+                state.inputAttitude = "text";
+            }
+            else if (state.inputAttitude === "text"){
+                state.inputAttitude = "password";
+            }
         default:
             return  state;
     }
@@ -27,5 +36,11 @@ export function createPasswordChangeAction(newPassword) {
     return {
         type : CHANGE_PASSWORD_INPUT_ACTION,
         newText : newPassword,
+    }
+}
+export function createChangeVisibleAction() {
+    return {
+        type : CHANGE_VISIBLE_ACTION,
+
     }
 }
