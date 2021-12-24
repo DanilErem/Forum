ca19a738141e82d3bd81339a4386fb011c51743c
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 19:59:34 +0200

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,1 +1,7 @@
-le
+import {combineReducers, createStore} from "redux";
+import {registrationReducer} from "./registration-reducer";
+
+let combinedReducers = combineReducers({registrationPage : registrationReducer});
+let store = createStore(combinedReducers);
+
+export default store;
