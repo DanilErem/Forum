4fd3fa8186f06b23e3aa42ba6d3f3eed4f308a05
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 19:24:10 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Regestration from "./Regestration/Regestration";
+import React from "react";
 function App() {
   return (
     <div className="App">
-
+      <Regestration/>
     </div>
   );
 }
