22772c66f72be69136c9f49194b90612d3afe62f
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 19:59:30 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Regestration from "./Regestration/Regestration";
 import React from "react";
+import store from "./store/store";
 function App() {
   return (
     <div className="App">
-      <Regestration/>
+      <Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>
     </div>
   );
 }
