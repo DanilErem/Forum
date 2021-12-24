5a9b4ffbb16c66cfd510545a33700436e6be3e58
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 20:16:12 +0200

    Update Regestration.js

--- frontend/src/Regestration/Regestration.js
+++ frontend/src/Regestration/Regestration.js
@@ -1,6 +1,10 @@
 import React, {createRef} from "react";
 import './Regestration.css'
-import {createNickNameChangeTextAction, createPasswordChangeAction} from "../store/registration-reducer";
+import {
+    createChangeVisibleAction,
+    createNickNameChangeTextAction,
+    createPasswordChangeAction
+} from "../store/registration-reducer";
 class Regestration extends React.Component {
     constructor(props) {
         super(props);
@@ -11,16 +15,22 @@ class Regestration extends React.Component {
         let action = createNickNameChangeTextAction(this.nickNameInput.current.value);
         this.props.dispatch(action);
     }
+    changeVisible(){
+        let action = createChangeVisibleAction();
+        this.props.dispatch(action);
+    }
     onPasswordChange(){
         let action = createPasswordChangeAction(this.passwordInput.current.value);
         this.props.dispatch(action);
     }
     render() {
+        debugger;
         return (
             <div className={"regestrarion-container"}>
                 <form className={"inputs-container"}>
-                    <input ref={this.nickNameInput} onChange={this.onNickNameChange.bind(this)} value={this.props.state.nicknameInputText} className={"regestration-input"} placeholder={"Nickname"}/>
-                    <input ref={this.passwordInput} onChange={this.onPasswordChange.bind(this)} value={this.props.state.passwordInputText} type={"password"} className={"regestration-input"} placeholder={"Password"} />
+                    <input  ref={this.nickNameInput} onChange={this.onNickNameChange.bind(this)} value={this.props.state.nicknameInputText} className={"regestration-input"} placeholder={"Nickname"}/>
+                    <input name={"password"} ref={this.passwordInput} onChange={this.onPasswordChange.bind(this)} value={this.props.state.passwordInputText} type={this.props.state.inputAttitude} className={"regestration-input"} placeholder={"Password"} />
+                    <label className={"visible-password"} onClick={this.changeVisible.bind(this)} for={"password"}>H</label>
                     <input className={"login"} type={"submit"} value={"Login"}/>
                 </form>
             </div>
