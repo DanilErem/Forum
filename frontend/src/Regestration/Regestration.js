6041cac9978daa633e9879130be3f1c461c96478
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 20:47:35 +0200

    Update Regestration.js

--- frontend/src/Regestration/Regestration.js
+++ frontend/src/Regestration/Regestration.js
@@ -28,7 +28,7 @@ class Regestration extends React.Component {
         return (
             <div className={"regestrarion-container"}>
                 <form className={"inputs-container"}>
-                    <input  ref={this.nickNameInput} onChange={this.onNickNameChange.bind(this)} value={this.props.state.nicknameInputText} className={"regestration-input"} placeholder={"Nickname"}/>
+                    <input name={"nickname"}  ref={this.nickNameInput} onChange={this.onNickNameChange.bind(this)} value={this.props.state.nicknameInputText} className={"regestration-input"} placeholder={"Nickname"}/>
                     <input name={"password"} ref={this.passwordInput} onChange={this.onPasswordChange.bind(this)} value={this.props.state.passwordInputText} type={this.props.state.inputAttitude} className={"regestration-input"} placeholder={"Password"} />
                     <label className={"visible-password"} onClick={this.changeVisible.bind(this)} htmlFor={"password"}><img src={this.props.state.passwordHideSrc} alt={""}/></label>
                     <input className={"login"} type={"submit"} value={"Login"}/>
