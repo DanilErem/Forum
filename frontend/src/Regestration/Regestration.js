a0dbadf3570794adc84719f21b2f556647ea4665
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 19:59:46 +0200

    Update Regestration.js

--- frontend/src/Regestration/Regestration.js
+++ frontend/src/Regestration/Regestration.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {createRef} from "react";
 import './Regestration.css'
+import {createNickNameChangeTextAction, createPasswordChangeAction} from "../store/registration-reducer";
 class Regestration extends React.Component {
+    constructor(props) {
+        super(props);
+        this.nickNameInput = createRef();
+        this.passwordInput = createRef();
+    }
+    onNickNameChange(){
+        let action = createNickNameChangeTextAction(this.nickNameInput.current.value);
+        this.props.dispatch(action);
+    }
+    onPasswordChange(){
+        let action = createPasswordChangeAction(this.passwordInput.current.value);
+        this.props.dispatch(action);
+    }
     render() {
         return (
             <div className={"regestrarion-container"}>
                 <form className={"inputs-container"}>
-                    <input className={"regestration-input"} placeholder={"Nickname"}/>
-                    <input type={"password"} className={"regestration-input"} placeholder={"Password"} />
+                    <input ref={this.nickNameInput} onChange={this.onNickNameChange.bind(this)} value={this.props.state.nicknameInputText} className={"regestration-input"} placeholder={"Nickname"}/>
+                    <input ref={this.passwordInput} onChange={this.onPasswordChange.bind(this)} value={this.props.state.passwordInputText} type={"password"} className={"regestration-input"} placeholder={"Password"} />
                     <input className={"login"} type={"submit"} value={"Login"}/>
                 </form>
             </div>
