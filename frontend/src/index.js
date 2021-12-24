0497659ebcef2069b1a87ca9d71c12e4500a92ec
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-24 19:59:53 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from "./store/store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+function rerender(){
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+rerender();
+store.subscribe(rerender);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
