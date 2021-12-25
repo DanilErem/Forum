31610c9322d3bb337833583564714c1b30328bb2
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 16:46:50 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Regestration from "./Regestration/Regestration";
 import React from "react";
 import store from "./store/store";
+import Navbar from "./Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>
+      {/*<Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>*/}
+      <Navbar/>
     </div>
   );
 }
