25753f2f866a97c274560be7b99d97e33ce84f27
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 20:11:14 +0200

    Create Navbar.js

---
+++ frontend/src/Component/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './Navbar.css'
+class Navbar extends React.Component{
+    render() {
+        return (
+            <div className={"navbar-container"}>
+                <div className={"navbar-element"}>
+
+                    <p className={"navbar-element-text"}>Chat</p>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
