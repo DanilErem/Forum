59df0752ac53963e3da1cd5f328d87cb5a6c6f52
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 19:57:43 +0200

    Create chats-reducer.js

---
+++ frontend/src/store/chats-reducer.js
@@ -0,0 +1,26 @@
+let chatsPage = {
+    chats : [
+        {
+            header : "Сколько будет 2 * 2 ?",
+            text : "ответ: блин, минимум 20 символов. если умножить\n" +
+                "\n" +
+                "(-2/3)^2\n" +
+                "\n" +
+                "4/9\n" +
+                "\n" +
+                "я не могу понять. 2 целые 1/3? или 2×1/3\n" +
+                "\n" +
+                "если 2 целые 1/3, то (-7/3)^2=49/9\n" +
+                "\n"
+        }
+    ]
+};
+const CHANGE_SITE = "CHANGE_SITE";
+export function chatsReducer(state = chatsPage, action) {
+    switch (action.type) {
+        case CHANGE_SITE:
+            return state;
+        default:
+            return state;
+    }
+}
