d9c0f37250a8b8cebd1e865f09f62752589e33b1
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 20:19:00 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import React from "react";
 import store from "./store/store";
 import Navbar from "./Component/Navbar/Navbar";
 import Chats from "./Component/Chats/Chats";
+import {Route, Routes} from "react-router";
 function App() {
   return (
+      <Routes>
     <div className="App">
       {/*<Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>*/}
       <Navbar/>
       <Chats chatePage={store.getState().chatsPage} dispatch={store.dispatch}/>
     </div>
+      </Routes>
   );
 }
 
