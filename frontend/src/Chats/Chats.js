a83177e8cc4e05f46751c459838146ab19207163
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 16:59:21 +0200

    Update Chats.js

--- frontend/src/Chats/Chats.js
+++ frontend/src/Chats/Chats.js
@@ -1,9 +1,13 @@
+import React from "react";
+import './Chats.css'
+import Chat from "./Chat/Chat";
 class Chats extends React.Component{
     render() {
         return (
             <div className={"chats-container"}>
-
+                <Chat/>
             </div>
         );
     }
 }
+export default Chats;
