e43ee67d1af242e89ccc649ed268fb1c126e93be
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 20:11:05 +0200

    Create Chat.js

---
+++ frontend/src/Component/Chats/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Chat.css'
+class Chat extends React.Component{
+    render() {
+        return (
+            <div className={"chat-container"}>
+                <div className={"chat-header"}>
+                    <p>Как пощитать масу зная плотность и объем ?</p>
+                </div>
+            </div>
+        )
+    }
+}
+export default Chat;
