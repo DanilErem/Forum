48cad79c6e7969f6d9d1bfc71e2e88d1c0ddfc7e
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 16:59:09 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import Regestration from "./Regestration/Regestration";
 import React from "react";
 import store from "./store/store";
 import Navbar from "./Navbar/Navbar";
+import Chats from "./Chats/Chats";
 function App() {
   return (
     <div className="App">
       {/*<Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>*/}
       <Navbar/>
+      <Chats/>
     </div>
   );
 }
