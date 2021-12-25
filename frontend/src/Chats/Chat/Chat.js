dc0c1d4c06b908f1c9b78157d40b85e22b7561ed
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-25 16:59:15 +0200

    Update Chat.js

---
+++ frontend/src/Chats/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Chat.css'
+class Chat extends React.Component{
+    render() {
+        return (
+            <div className={"chat-container"}>
+                <div className={"chat-header"}>
+
+                </div>
+            </div>
+        )
+    }
+}
+export default Chat;
