f18e53bad81abbf2cd98841b0cc3923957a42890
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-27 08:51:07 +0200

    Update chats-reducer.js

--- frontend/src/store/chats-reducer.js
+++ frontend/src/store/chats-reducer.js
@@ -1,17 +1,14 @@
 let chatsPage = {
     chats : [
         {
-            header : "Сколько будет 2 * 2 ?",
-            text : "ответ: блин, минимум 20 символов. если умножить\n" +
-                "\n" +
-                "(-2/3)^2\n" +
-                "\n" +
-                "4/9\n" +
-                "\n" +
-                "я не могу понять. 2 целые 1/3? или 2×1/3\n" +
-                "\n" +
-                "если 2 целые 1/3, то (-7/3)^2=49/9\n" +
-                "\n"
+            header : "Когда на отработку?",
+            text : "Извините, я очень хочу попасть на отработку, но увы я не знаю когда она. Можете пожалуйста сказать мне ?",
+            id : 0,
+            messages : [
+                {
+                    text : "На 9:30",
+                }
+            ]
         },
         {
             header : "Сколько будет 2 * 2 ?",
@@ -24,10 +21,21 @@ let chatsPage = {
                 "я не могу понять. 2 целые 1/3? или 2×1/3\n" +
                 "\n" +
                 "если 2 целые 1/3, то (-7/3)^2=49/9\n" +
-                "\n"
+                "\n",
+            messages: [
+
+            ],
+            id : 1,
         }
     ]
 };
+export function getCurrentChat(id){
+    for (let chat of chatsPage.chats){
+        if (chat.id === id){
+            return chat;
+        }
+    }
+}
 const CHANGE_SITE = "CHANGE_SITE";
 export function chatsReducer(state = chatsPage, action) {
     switch (action.type) {
