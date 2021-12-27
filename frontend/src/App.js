db967b43306132bf6dcf50053431ad3669fe0727
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-27 08:50:36 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,21 @@ import Chats from "./Component/Chats/Chats";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Regestration from "./Component/Regestration/Regestration";
 import Navbar from "./Component/Navbar/Navbar";
+import ChatContainer from "./Component/Chats/Chat/ChatContainer";
 function App() {
-  return (
-      <BrowserRouter location={""} navigator={""} history={""}>
-          <div className="App">
-               <Routes>
-                  <Route  path={"/"} element={<Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>}/>
-                   <Route path={"/chats"} element={<div><Navbar/><Chats dispatch={store.dispatch} chatPage={store.getState().chatsPage}/></div>}/>
-               </Routes>
-          </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter location={""} navigator={""} history={""}>
+            <div className="App">
+                <Routes>
+                    <Route  path={"/"} element={<Regestration dispatch={store.dispatch} state={store.getState().registrationPage}/>}/>
+                    <Route path={"/chats"} >
+                        <Route index element={<div><Navbar/><Chats dispatch={store.dispatch} chatPage={store.getState().chatsPage}/></div>}/>
+                        <Route path={":id"} element={<ChatContainer dispatch={store.dispatch} chatPage={store.getState().chatsPage}/>}/>
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
