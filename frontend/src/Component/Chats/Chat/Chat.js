38e6d47305dce129e69c20510e54ef52412999bc
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-27 08:50:44 +0200

    Update Chat.js

--- frontend/src/Component/Chats/Chat/Chat.js
+++ frontend/src/Component/Chats/Chat/Chat.js
@@ -1,11 +1,29 @@
 import React from "react";
 import './Chat.css'
+import Message from "./Message/Message";
 class Chat extends React.Component{
+    renderMessages(){
+        return this.props.chat.messages.map(message=>{
+            return (
+                <Message message={message}/>
+            )
+        });
+    }
     render() {
         return (
             <div className={"chat-container"}>
-                <div className={"chat-header"}>
-                    <p>Как пощитать масу зная плотность и объем ?</p>
+                <div className={"main-message-container"}>
+                    <div className={"main-message-header"}>
+                        <p className={"main-message-header-text"}>{this.props.chat.header}</p>
+                    </div>
+                    <div className={"main-message-body"}>
+                        <p className={"main-message-body-text"}>
+                            {this.props.chat.text}
+                        </p>
+                    </div>
+                </div>
+                <div className={"messages-container"}>
+                    {this.renderMessages()}
                 </div>
             </div>
         )
