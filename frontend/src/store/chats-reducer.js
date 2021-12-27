7989eb78b0b7c53fa5b5dd1b816989613c2cb021
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-27 16:09:38 +0200

    Update chats-reducer.js

--- frontend/src/store/chats-reducer.js
+++ frontend/src/store/chats-reducer.js
@@ -1,3 +1,5 @@
+export const VIEW_REGIME = "VIEW";
+export const ADD_CHAT_REGIME = "ADD_CHAT";
 let chatsPage = {
     chats : [
         {
@@ -12,23 +14,32 @@ let chatsPage = {
         },
         {
             header : "Сколько будет 2 * 2 ?",
-            text : "ответ: блин, минимум 20 символов. если умножить\n" +
-                "\n" +
-                "(-2/3)^2\n" +
-                "\n" +
-                "4/9\n" +
-                "\n" +
-                "я не могу понять. 2 целые 1/3? или 2×1/3\n" +
-                "\n" +
-                "если 2 целые 1/3, то (-7/3)^2=49/9\n" +
-                "\n",
+            text : "",
             messages: [
-
+                {
+                    text : "ответ: блин, минимум 20 символов. если умножить\n" +
+                        "\n" +
+                        "(-2/3)^2\n" +
+                        "\n" +
+                        "4/9\n" +
+                        "\n" +
+                        "я не могу понять. 2 целые 1/3? или 2×1/3\n" +
+                        "\n" +
+                        "если 2 целые 1/3, то (-7/3)^2=49/9\n" +
+                        "\n",
+                }
             ],
             id : 1,
         }
-    ]
+    ],
+    chatCreator : {
+        text : "",
+    },
+    regime : VIEW_REGIME,
 };
+const CHANGE_REGIME_ACTION = "CHANGE_STATE_ACTION";
+const CHANGE_TEXT_ACTION = "CHANGE_TEXT";
+
 export function getCurrentChat(id){
     for (let chat of chatsPage.chats){
         if (chat.id === id){
@@ -36,12 +47,28 @@ export function getCurrentChat(id){
         }
     }
 }
-const CHANGE_SITE = "CHANGE_SITE";
+
 export function chatsReducer(state = chatsPage, action) {
     switch (action.type) {
-        case CHANGE_SITE:
+        case CHANGE_REGIME_ACTION:
+            state.regime = action.regime;
             return state;
+        case CHANGE_TEXT_ACTION:
+            state.chatCreator.text = action.newText;
+            return  state;
         default:
             return state;
+    }
+}
+export function createChangeRegimeAction(REGIME) {
+    return {
+        regime : REGIME,
+        type : CHANGE_REGIME_ACTION,
+    }
+}
+export function createChangeTextAction(newText) {
+    return {
+        type : CHANGE_TEXT_ACTION,
+        newText :  newText,
     }
 }
