5ec8de22e827ef797fbaec90121759f73389bef7
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-27 11:25:08 +0200

    Update Chats.js

--- frontend/src/Component/Chats/Chats.js
+++ frontend/src/Component/Chats/Chats.js
@@ -1,21 +1,26 @@
 import React from "react";
 import './Chats.css'
 import {useNavigate} from "react-router-dom";
+import {ADD_CHAT_REGIME, createChangeRegimeAction} from "../../store/chats-reducer";
+import CreatorChat from "./CreateChatWindow/CreatorChat";
 
 function Chats (props){
     let navigate = useNavigate();
-
-
+    function createAddChatWindow() {
+        let action = createChangeRegimeAction(ADD_CHAT_REGIME);
+        props.dispatch(action);
+    }
     function renderChatLink(chat){
         function onClick() {
             navigate(`/chats/${chat.id}`)
         }
         return (
-            <div  onClick={onClick} className={"chat-link-container"}>
-                <div className={"chat-link-header"}>
-                    <p>{chat.header}</p>
+                <div   className={"chat-link-container"}>
+                    <div className={"chat-link-header"}>
+                        <p onClick={onClick}>{chat.header}</p>
+                    </div>
                 </div>
-            </div>
+
         )
     }
     function renderChatLinks(){
@@ -23,12 +28,19 @@ function Chats (props){
             return renderChatLink(chat);
         });
     }
+    function renderChatCreator() {
+        if (props.chatPage.regime === ADD_CHAT_REGIME) {
+            return (<CreatorChat/>)
+        }
+    }
+    return (
+        <div className={"chats-container"}>
 
-        return (
-            <div className={"chats-container"}>
-                {renderChatLinks()}
-            </div>
-        );
+            {renderChatLinks()}
+            <button onClick={createAddChatWindow} className={"add-post-button"}/>
+            {renderChatCreator()}
+        </div>
+    );
 
 }
 export default (Chats);
