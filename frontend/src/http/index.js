26cdfa32f795a2060be51637f33832e612dda920
DESKTOP-UAKANTS\DanilErem <danilerem@gmail.com>
2021-12-27 19:40:06 +0300

    Add user api on client

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
+const API_URL = 'http://localhost:8000'
+
+
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: API_URL
 });
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: API_URL
 });
 
 const authInterceptor = config => {
