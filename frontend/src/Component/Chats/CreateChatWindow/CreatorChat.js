21ec23e3ce323bf901d8e2d39761a869a49379a2
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-27 16:09:34 +0200

    Update CreatorChat.js

--- frontend/src/Component/Chats/CreateChatWindow/CreatorChat.js
+++ frontend/src/Component/Chats/CreateChatWindow/CreatorChat.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {createRef} from "react";
 import './CreatorChat.css';
+import {createChangeTextAction} from "../../../store/chats-reducer";
 class CreatorChat extends React.Component{
+    constructor(props) {
+        super(props);
+        this.textArea = createRef();
+    }
+
+    onTextEnter(){
+        if (this.textArea.current !== null) {
+            let action = createChangeTextAction(this.textArea.current.value);
+            this.props.dispatch(action);
+        }
+    }
+
     render() {
 
         return (
             <div>
-                < div className={"creator-chat-background"}/>
+                <div className={"creator-chat-background"}/>
                 <div className={"creator-chat-container"}>
-                    <textarea value={this.props.chatCreator.text} className={"creator-chat-input"}/>
+                    <textarea ref={this.textArea} onChange={this.onTextEnter.bind(this)} value={this.props.chatCreator.text} className={"creator-chat-input"}/>
                 </div>
             </div>
 
