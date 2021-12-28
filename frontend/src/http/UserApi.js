e5d906a684b41643067761ba33fa93b303a7abc1
DESKTOP-UAKANTS\DanilErem <danilerem@gmail.com>
2021-12-28 21:02:45 +0300

    Updata user api

--- frontend/src/http/UserApi.js
+++ frontend/src/http/UserApi.js
@@ -3,18 +3,18 @@ import jwt_decode from 'jwt-decode';
 
 export const registration = async (email, password) => {
     const data = await $host.post('api/user/registration', {email, password});
-    localStorage.setItem('token', data);
-    return jwt_decode(data);
+    localStorage.setItem('token', data.data);
+    return jwt_decode(data.data);
 }
 
 export const login = async (email, password) => {
     const data = await $host.post('api/user/login', {email, password});
-    localStorage.setItem('token', data);
-    return jwt_decode(data);
+    localStorage.setItem('token', data.data);
+    return jwt_decode(data.data);
 }
 
 export const check = async () => {
     const data = await $authHost.get('api/user/auth');
-    localStorage.setItem('token', data);
-    return jwt_decode(data);
+    localStorage.setItem('token', data.data);
+    return jwt_decode(data.data);
 }
