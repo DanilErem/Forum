d43299090baa8bc10ab1835adb6e8e85c820aed3
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-29 10:27:42 +0200

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import {registrationReducer} from "./registration-reducer";
 import {chatsReducer} from "./chats-reducer";
+import {userReducer} from "./user-reducer";
 
 let combinedReducers = combineReducers({
     registrationPage : registrationReducer,
     chatsPage  : chatsReducer,
+    usersReducer : userReducer,
 });
 let store = createStore(combinedReducers);
 
