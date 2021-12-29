b2432f0cd69bbd54a4e6a6533fd0f8b0a43edfc4
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-29 18:53:41 +0200

    Update ChatContainer.js

--- frontend/src/Component/Chats/Chat/ChatContainer.js
+++ frontend/src/Component/Chats/Chat/ChatContainer.js
@@ -1,14 +1,14 @@
 
 import {useParams} from "react-router-dom";
-import {getCurrentChat} from "../../../store/chats-reducer";
+import {getChatById} from "../../../store/chats-reducer";
 import Chat from "./Chat";
 import React from "react";
 function ChatContainer (props){
     let params = useParams();
     function renderCurrentChat(){
-      let currentChat = getCurrentChat(parseInt(params.id, 10));
+      let currentChat = getChatById(parseInt(params.id, 10));
       if (currentChat !== undefined){
-          return <Chat chat={currentChat} />
+          return <Chat dispatch={props.dispatch} chat={currentChat} />
        }
 
     }
