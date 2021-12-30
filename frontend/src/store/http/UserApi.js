6acaebc841267f32948de7eb7736ae947da1f80b
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2021-12-30 10:18:44 +0200

    Update UserApi.js

--- frontend/src/store/http/UserApi.js
+++ frontend/src/store/http/UserApi.js
@@ -7,12 +7,7 @@ export const registration = async (email, password) => {
 
     return jwt_decode(data.data);
 }
-export const getUsers = async ()=>{
-    const data = await $host.get('api/user/registration');
-    localStorage.setItem('token', data.data);
 
-    return jwt_decode(data.data);
-}
 export const login = async (email, password) => {
     const data = await $host.post('api/user/login', {email, password});
     localStorage.setItem('token', data.data);
