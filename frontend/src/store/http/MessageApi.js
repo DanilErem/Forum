453152513bea2f295bd6030ac795af7fc81390e5
Gleb2006p <74091897+Gleb2006p@users.noreply.github.com>
2022-01-01 20:42:52 +0200

    Update MessageApi.js

--- frontend/src/store/http/MessageApi.js
+++ frontend/src/store/http/MessageApi.js
@@ -1,49 +1,47 @@
 import {$authHost} from './index'
 
-export const create = async (owner, text, chat) => {
-    return $authHost.post('api/message', {owner, text, chat}).then(res => {
-        return res.data
-    }).catch(e => {
-        return e.response.data
-    })
-}
-
-export const getAll = async () => {
-    return $authHost.get('api/message').then(res => {
-        return res.data
-    }).catch(e => {
-        return e.response.data
-    })
-}
-
-export const getOne = async (id) => {
-    return $authHost.get(`api/message/${id}`).then(res => {
-        return res.data
-    }).catch(e => {
-        return e.response.data
-    })
-}
-
-export const getAllByUser = async (id) => {
-    return $authHost.get(`api/message/user/${id}`).then(res => {
-        return res.data
-    }).catch(e => {
-        return e.response.data
-    })
-}
-export const getAllByChat = async (id) => {
-    return $authHost.get(`api/message/chat/${id}`).then(res => {
-        return res.data
-    }).catch(e => {
-        return e.response.data
-    })
-}
-
-export const del = async (id) => {
-    return $authHost.delete(`api/message/${id}`).then(res => {
-        return res.data
-    }).catch(e => {
-        return e.response.data
-    })
+export let messageApi = {
+    create : async (owner, text, chat) => {
+        return $authHost.post('api/message', {owner, text, chat}).then(res => {
+            return res.data
+        }).catch(e => {
+            return e.response.data
+        })
+    },
+    getAll : async () => {
+        return $authHost.get('api/message').then(res => {
+            return res.data
+        }).catch(e => {
+            return e.response.data
+        })
+    },
+    getOne : async (id) => {
+        return $authHost.get(`api/message/${id}`).then(res => {
+            return res.data
+        }).catch(e => {
+            return e.response.data
+        })
+    },
+    getAllByUser : async (id) => {
+        return $authHost.get(`api/message/user/${id}`).then(res => {
+            return res.data
+        }).catch(e => {
+            return e.response.data
+        })
+    },
+    getAllByChat : async (id) => {
+        return $authHost.get(`api/message/chat/${id}`).then(res => {
+            return res.data
+        }).catch(e => {
+            return e.response.data
+        })
+    },
+    del :  async (id) => {
+        return $authHost.delete(`api/message/${id}`).then(res => {
+            return res.data
+        }).catch(e => {
+            return e.response.data
+        })
+    }
 }
 
